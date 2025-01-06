fa1db357e1614fedad01a644c5e9fe4e2f57c848
Edwin Ekpenyong <moogiemode@gmail.com>
2025-01-05 18:11:34 -0800

    next ui added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { NextUIProvider } from '@nextui-org/system'
+import { Button } from '@nextui-org/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <NextUIProvider>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -18,18 +20,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button onPress={() => setCount((count) => count + 1)} color="primary">count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </NextUIProvider>
+  );
 }
 
 export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const { nextui } = require('@nextui-org/theme');
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}', './node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [nextui()],
+};
